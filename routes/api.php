45856a805dd5e7c4f15bbb9c6705a6e019e87018
aloisjunior <aloisjunior@users.noreply.github.com>
2020-09-11 08:44:56 -0300

    -Inclusão de autenticação Sanctum, pré intalação de UI para registro de usuários

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/login/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/agenda', 'AgendaController@index')
+    ->name('agenda.index');
+
+Route::middleware('auth:sanctum')->get('/agenda/listar', 'AgendaController@index')
+    ->name('agenda.index.listar');
+Route::middleware('auth:sanctum')->post('/agenda', 'AgendaController@store')
+    ->name('agenda.store');
+Route::middleware('auth:sanctum')->post('/agenda/{id}/update', 'AgendaController@update')
+    ->name('agenda.update');
+Route::middleware('auth:sanctum')->get('/agenda/{id}/update', 'AgendaController@edit') // load de registro para atualizar
+->name('agenda.edit');
+
+Route::middleware('auth:sanctum')->get('/agenda/{id}/destroy', 'AgendaController@destroy')
+    ->name('agenda.destroy');
+
+
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
